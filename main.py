eb30845386ccafb6d0432612b2749030e2157d57
B1NND <alexandr151215@gmail.com>
2022-10-10 18:30:29 +0300

    new function

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import requests
 
-url = "https://wttr.in/%D0%A1%D0%B0%D0%BD%D0%BA%D1%82-%D0%9F%D0%B5%D1%82%D0%B5%D1%80%D0%B1%D1%83%D1%80%D0%B3"
-
-response = requests.get(url)
-response.raise_for_status()
-print(response.text)
+print("Введите свой город")
+City = input()
+url = f"https://wttr.in/{City}"
+responce = requests.get(url)
+responce.raise_for_status()
+print(responce.text)
